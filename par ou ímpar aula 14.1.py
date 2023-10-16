2b0889cd9743e7bf37384ee36ff011328c1ca560
Samuel260107 <130668952+Samuel260107@users.noreply.github.com>
2023-10-16 14:34:37 -0300

    -Commit inicial do tio.

---
+++ par ou ímpar aula 14.1.py
@@ -0,0 +1,16 @@
+#Projeto Par ou Impar
+
+#Apresentação
+
+print('\n\t\t\t -- Verificador de Numero \n\n')
+
+#Entradas
+num=int(input('Informe um numero:'))
+
+#Processamento e Saída
+if (num % 2) == 0:
+    print(f'\n{num} e par!')
+else:
+    print(f'\n{num} e impar!')
+
+
